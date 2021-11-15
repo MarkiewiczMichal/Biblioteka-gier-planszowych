12adf4ad0d78ec85a177fe73f893d898d07096d0
MarkiewiczMichal <michal0markiewicz@gmail.com>
2021-11-15 21:39:18 +0100

    dodano Springa

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        SpringApplication.run(Main.class, args);
 
-        System.out.println("kolejny glupi tekst");
-        System.out.println("kolejny glupi tekst");
-        System.out.println("kolejny glupi tekst");
     }
 }
---
+++ src/test/java/com/example/bibliotekagierplanszowych/BibliotekaGierPlanszowychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotekagierplanszowych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaGierPlanszowychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
