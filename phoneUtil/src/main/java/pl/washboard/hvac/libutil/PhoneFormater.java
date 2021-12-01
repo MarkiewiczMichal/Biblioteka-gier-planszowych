02c6ea12976f800cd9225f2e687cb705721f16dc
MarkiewiczMichal <michal0markiewicz@gmail.com>
2021-12-01 23:27:02 +0100

    dodano endpoint international

---
+++ phoneUtil/src/main/java/pl/washboard/hvac/libutil/PhoneFormater.java
@@ -0,0 +1,28 @@
+package pl.washboard.hvac.libutil;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
+public class PhoneFormater {
+    private static final PhoneNumberUtil PHONE_NUMBER_UTIL = PhoneNumberUtil.getInstance();
+
+    public String national(String str) {
+        try {
+            Phonenumber.PhoneNumber NumberProto = PHONE_NUMBER_UTIL.parse(str, "PL");
+            return PHONE_NUMBER_UTIL.format(NumberProto, PhoneNumberUtil.PhoneNumberFormat.NATIONAL);
+        } catch (NumberParseException e) {
+            return str;
+        }
+    }
+
+    public String international(String str) {
+        try {
+            Phonenumber.PhoneNumber NumberProto = PHONE_NUMBER_UTIL.parse(str, "PL");
+            return PHONE_NUMBER_UTIL.format(NumberProto, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+        } catch (NumberParseException e) {
+            return str;
+        }
+    }
+
+}
