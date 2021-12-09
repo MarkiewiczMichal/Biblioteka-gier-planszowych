d7b159125ac0d5b0c2b3cdd7b0e509dd9e757710
MarkiewiczMichal <michal0markiewicz@gmail.com>
2021-12-09 22:11:02 +0100

    dwa endpointy tylko dla zalogowanych

--- src/main/java/pl/washboard/hvac/gamelibrary/configuration/CustomWebSecurityConfigurerAdapter.java
+++ src/main/java/pl/washboard/hvac/gamelibrary/configuration/CustomWebSecurityConfigurerAdapter.java
@@ -29,7 +29,7 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/hello").permitAll()
+                .antMatchers("/format/national/{phoneNumber}").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
