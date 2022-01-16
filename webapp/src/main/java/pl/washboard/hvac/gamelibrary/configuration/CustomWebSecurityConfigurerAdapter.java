470065a112fde378cd9ebba0d4d93557a112f202
MarkiewiczMichal <michal0markiewicz@gmail.com>
2022-01-16 15:06:15 +0100

    wprowadzono poprawki na podstawie uwag

--- webapp/src/main/java/pl/washboard/hvac/gamelibrary/configuration/CustomWebSecurityConfigurerAdapter.java
+++ webapp/src/main/java/pl/washboard/hvac/gamelibrary/configuration/CustomWebSecurityConfigurerAdapter.java
@@ -3,6 +3,7 @@ package pl.washboard.hvac.gamelibrary.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,10 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationEn
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Objects;
+
 @Configuration
 @EnableWebSecurity
 public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
@@ -33,17 +38,22 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
                 .authorities("ROLE_USER");
     }
 
+    @Autowired
+    private Environment environment;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/", "/nologin", "/auth/userinfo").permitAll()
-                .anyRequest().authenticated()
-                .and()
-//                .httpBasic()
-//                .authenticationEntryPoint(authenticationEntryPoint);
-        .oauth2Login();
-        http
-                .logout()
+        var builder = http.authorizeRequests()
+                .antMatchers("/", "/nologin", "/auth/userinfo", "/dupa").permitAll()
+                .anyRequest().authenticated();
+        if (Arrays.asList(environment.getActiveProfiles()).contains("oauth")) {
+            builder.and().oauth2Login();
+        } else {
+            builder.and().httpBasic()
+                    .authenticationEntryPoint(authenticationEntryPoint);
+        }
+
+        http.logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/nologin");
 
--- webapp/src/main/java/pl/washboard/hvac/gamelibrary/controller/IndexController.java
+++ webapp/src/main/java/pl/washboard/hvac/gamelibrary/controller/IndexController.java
@@ -3,11 +3,9 @@ package pl.washboard.hvac.gamelibrary.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
-
 
     @GetMapping("nologin")
     public String noLoginPage(Model model) {
@@ -17,10 +15,5 @@ public class IndexController {
     @GetMapping("hello")
     public String sayHello(Model model) {
         return "hello";
-    }
-
-    @GetMapping("auth/userinfo")
-    public String userInformation(Model model) {
-        return "aboutUserInfo";
     }
 }
